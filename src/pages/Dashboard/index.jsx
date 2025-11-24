06d04ac42197d8148debcfc8aa46bcf322ddd6a2
muryllo_henrique <muryllosaber@hotmail.com>
2025-11-24 19:14:28 -0300

    Feat: adiçao de perfil Dashboard
    Foi adicionado o nome do usuário  e a imagem do seu avatar no lado superior direito da aba Dashboard, desta forma deixando mais único cada dashboard de usuario

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,16 +1,32 @@
 import { useNavigate } from 'react-router-dom'
 import './style.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function Dashboard() {
     const navigate = useNavigate()
     const usuario = JSON.parse(localStorage.getItem("usuario"));
+    const [MenuPerfil, setMenuPerfil ] = useState (false);
+    
     useEffect(()=>{
         if(!usuario){
             navigate("/");
         }
     })
+    
+    function Menu(){
+        
+        return (
+            <div className="MenuPerfil">
+                <p>Perfil</p>
+                <p>Configurações</p>
+                <p>Foruns</p>
+                <p onClick={() => {
+                    navigate('/');
+                }}>Sair</p>
+            </div>
+        );
+    }
     const cards = [
         { id: 1, title: "Hardware", description: "Ajuda com PCs e peças." },
         { id: 2, title: "Programação", description: "Dúvidas de código." },
@@ -25,14 +41,15 @@ function Dashboard() {
                 <h1 className='Dash-logo1'>Tech</h1>
                 <div className='Dash-usuario'>
                     <img className='Dash-avatar' src={usuario?.avatar} alt="avatar"></img>
-                    <p className='Dash-perfil'>{usuario?.nome}</p>
+                    <p className='Dash-perfil' onClick={()=>setMenuPerfil(!MenuPerfil)} style={{cursor: 'pointer'}}>{usuario?.nome}</p> 
+                    {MenuPerfil && Menu()} 
                 </div>
                 <p className='Dash-logo2'>4Um</p>
                 <p className='Dash-inicio'>Opa!! <br />Sobre o que gostaria de falar hoje?</p>
                 <div className='Dash-row'>
                     <input name='Dash-busca' type='text' />
                     <button type='button' onClick={() => navigate('/Chat')}>→</button>
-                    <button type='button' className='Dash-botao2'>Ou crie seu próprio 4um</button></div>
+                    <button type='button' className='Dash-botao2' onClick={()=> navigate('/Foruns')}>Ou crie seu próprio 4um</button></div>
             </form>
             <div className='Card-container'>{cards.map(card=>(
                 <div key={card.id} className='Card' onClick={()=>navigate('/Chat/${card.id}')}>
