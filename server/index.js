cfaef99b887dc6584a69f7478cdd63c2c99e45cf
Joao0liver <joaoolv.profissional@gmail.com>
2025-11-25 19:54:22 -0300

    feat: Criação do Model e Controller dos Fóruns

--- server/index.js
+++ server/index.js
@@ -6,17 +6,21 @@ const cors = require('cors');
 // Importa os Controllers e Models
 const UsuarioController = require('./controller/UsuarioController.js');
 const UsuarioModel = require('./model/UsuarioModel.js');
+const ForumController = require('./controller/ForumController.js');
+const ForumModel = require('./model/ForumModel.js');
 
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 UsuarioModel.criarTabela();
+ForumModel.criarTabela();
 
 // Rotas da API
 app.post('/api/usuario-cadastro', UsuarioController.cadastrar);
 app.post('/api/usuario-login', UsuarioController.login);
+app.post('/api/forum-cadastro', ForumController.cadastrar);
+app.get('/api/foruns', ForumController.listar);
+app.get('/api/foruns/:nome', ForumController.buscar);
 
 const server = http.createServer(app);
 const PORT = 3001
