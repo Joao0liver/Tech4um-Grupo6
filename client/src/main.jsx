d692ee5a36daffe4fbd067348854e6eb36db9040
Joao0liver <joaoolv.profissional@gmail.com>
2025-11-25 15:11:30 -0300

    fix + refactor: Correção do cadastro de usuário e estrutura de componentes App/main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import Chat from './pages/Chat'
-import './index.css'
-import Cadastro from './pages/Cadastro'
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Cadastro" element={<Cadastro />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-        <Route path ="/Chat" element={<Chat/>}/>
-      </Routes>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
