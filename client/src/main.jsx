91454816bcb89bdc53b602c7f5011f538b1b85d9
Joao0liver <joaoolv.profissional@gmail.com>
2025-11-25 14:38:30 -0300

    fix: Correção no redirecionamento para a tela de cadastro

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,12 +6,14 @@ import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Chat from './pages/Chat'
 import './index.css'
+import Cadastro from './pages/Cadastro'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Cadastro" element={<Cadastro />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path ="/Chat" element={<Chat/>}/>
       </Routes>
